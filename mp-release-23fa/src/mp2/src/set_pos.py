3af7df9e9dd958a0f38ef2cc8a6b478c8764fcd2
Andrew Chang <21andrewch@gmail.com>
2024-02-29 05:21:42 -0600

    fix: removed old mps

--- mp-release-23fa/src/mp2/src/set_pos.py
+++ mp-release-23fa/src/mp2/src/set_pos.py
@@ -9,6 +9,8 @@ from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.srv import GetModelState
 
+from util import euler_to_quaternion
+
 def getModelState():
     rospy.wait_for_service('/gazebo/get_model_state')
     try:
@@ -26,45 +28,49 @@ def setModelState(model_state):
     except rospy.ServiceException as e:
         rospy.loginfo("Service did not process request: "+str(e))
 
-def euler_to_quaternion(r):
-    (yaw, pitch, roll) = (r[0], r[1], r[2])
-    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-    return [qx, qy, qz, qw]
-
-def set_position(x = 0,y = 0):
+def set_position(x = 0,y = 0, yaw=0):
     
     rospy.init_node("set_pos")
 
     curr_state = getModelState()
     new_state = ModelState()
     new_state.model_name = 'gem'
-    new_state.pose = curr_state.pose
+    new_state.twist.linear.x = 0
+    new_state.twist.linear.y = 0
+    new_state.twist.linear.z = 0
     new_state.pose.position.x = x
     new_state.pose.position.y = y
     new_state.pose.position.z = 1
-    q = euler_to_quaternion([0,0,0])
+    q = euler_to_quaternion([0,0,yaw])
     new_state.pose.orientation.x = q[0]
     new_state.pose.orientation.y = q[1]
     new_state.pose.orientation.z = q[2]
     new_state.pose.orientation.w = q[3]
-    new_state.twist = curr_state.twist
+    new_state.twist.angular.x = 0
+    new_state.twist.angular.y = 0
+    new_state.twist.angular.z = 0
     setModelState(new_state)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Set the x, y position of the vehicle')
 
     x_default = 0
-    y_default = 0
+    y_default = -98
+    yaw_default = 0
+
+    # x_default = 160
+    # y_default = 96.5
+    # yaw_default = 3.14
+
 
     parser.add_argument('--x', type = float, help = 'x position of the vehicle.', default = x_default)
     parser.add_argument('--y', type = float, help = 'y position of the vehicle.', default = y_default)
+    parser.add_argument('--yaw', type = float, help = 'yaw of the vehicle.', default = yaw_default)
 
     argv = parser.parse_args()
 
     x = argv.x
     y = argv.y
+    yaw = argv.yaw
 
-    set_position(x = x, y = y)
+    set_position(x = x, y = y, yaw = yaw)
